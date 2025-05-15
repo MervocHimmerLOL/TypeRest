bb39bf8b96586e5124181d4236bfbc61fe85c864
Михаил Меренцов <flash00052@gmail.com>
2025-05-15 17:56:03 +0300

    Add Dish model

--- type_rest_django/restaurant/models.py
+++ type_rest_django/restaurant/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Dish(models.Model):
+    name = models.CharField("Название",max_length=40)
+    description = models.TextField("Описание")
+    price = models.DecimalField("Цена", max_digits=8, decimal_places=2, default=0.00)
+    category = models.CharField("Категория", max_length=40)
